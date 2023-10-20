4c9d390618b8ce612e5438343750048e8b75faa0
ShadowJS3 <luisgabriel8328@gmail.com>
2023-10-20 17:18:09 -0400

    localstoraghe finalizado

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,44 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ima2 from './ima2.png';
 import './style.css';
 
 export default function Todo() {
-  const [lista, setLista] = useState([]);
+  const listalocalstorage = JSON.parse(localStorage.getItem("lista")) || [];
+  const [lista, setLista] = useState(listalocalstorage);
   const [id, setid] = useState(1);
   const [herois, setHerois] = useState("");
-  const [poder, setPoder] = useState("")
+  const [poder, setPoder] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("lista", JSON.stringify(lista));
+  }, [lista]);
+
   function salvar(e) {
     e.preventDefault();
-    setLista([...lista, { herois: herois, id: id, poder: poder}]);
+    setLista([...lista, { herois: herois, id: id, poder: poder }]);
     setid(id + 1);
   }
+
   const remover = (id) => {
-    const listaAux = [];
-    lista.map((heroi) => {
-      if (heroi.id !== id) {
-        listaAux.push(heroi);
-      }
-    })
+    const listaAux = lista.filter((heroi) => heroi.id !== id);
     setLista(listaAux);
   };
+
   return (
     <div className="peblou">
       <h1>Herois</h1>
       <Link to="/home">
-        <img src={ima2} alt="Imagem" width="100" height="50"></img>
+        <img src={ima2} alt="Imagem" width="100" height="50" />
       </Link>
       <form onSubmit={salvar}>
         <input onChange={(e) => setHerois(e.target.value)} type="text" />
         <input onChange={(e) => setPoder(e.target.value)} type="number" />
-        <button>Encrementa</button>
+        <button>Incrementar</button>
       </form>
-      {lista.map((herois) => (
-        <div key={herois.id}>
-          <p>{herois.id}</p>
-          <p>{herois.herois}</p>
-          <p>{herois.poder}</p>
-          <button onClick={(e) => remover(herois.id)}>Apagar</button>
+      {lista.map((heroi) => (
+        <div key={heroi.id}>
+          <p>ID: {heroi.id}</p>
+          <p>Herói: {heroi.herois}</p>
+          <p>Poder: {heroi.poder}</p>
+          <button onClick={() => remover(heroi.id)}>Apagar</button>
         </div>
       ))}
     </div>
